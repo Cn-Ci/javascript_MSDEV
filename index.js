d78cd7472b26561e18c9de87ab1afaad01b0ba9d
Cn-Ci <johndoe@example.comcindy.cnockaert@live.fr>
2021-09-27 16:17:58 +0200

    bases

---
+++ index.js
@@ -0,0 +1,17 @@
+// alert('Hello World');
+// ["Welcome !", "How are you ?"].forEach(alert);
+
+console.log(12%5);
+
+let x = 1;
+let  = 2; 
+
+/* affectation */                           x = y;       x = y;
+/* affectation après addition */            x += y;      x = x + y;
+/* affectation après soustraction */        x -= y;      x = x - y;
+/* affectation après multiplication */      x *= y;      x = x * y;
+/* affectation après division */            x /= y;      x = x / y;
+/* affectation du reste */                  x %= y;      x = x % y;
+/* affectation après exponentiation */      x **= y;     x = x ** y;
+
+
